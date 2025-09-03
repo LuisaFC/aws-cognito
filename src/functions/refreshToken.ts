9005494d813c6bf571914275bfd3ba2f7561735d
LuisaFC <luisabsb@gmail.com>
2025-09-03 14:11:13 -0300

    refresh token

---
+++ src/functions/refreshToken.ts
@@ -0,0 +1,37 @@
+import { bodyParser } from '@/utils/bodyParses';
+import { response } from '@/utils/response';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { InitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { cognitoClient } from '@/libs/cognitoClient';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const {refreshToken} = bodyParser(event.body);
+
+    const command = new InitiateAuthCommand({
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      AuthFlow: 'REFRESH_TOKEN_AUTH',
+      AuthParameters: {
+        REFRESH_TOKEN: refreshToken
+      }
+    });
+
+    const {AuthenticationResult} = await cognitoClient.send(command);
+
+    if(!AuthenticationResult){
+      return response(401, {
+        error: 'Invalid Credentials'
+      });
+    }
+
+    return response(200, {
+      accessToken: AuthenticationResult.AccessToken,
+      refreshToken: AuthenticationResult.RefreshToken,
+    });
+
+  } catch{
+    return response(500, {
+      error: 'Internal Server Error'
+    });
+  }
+}
